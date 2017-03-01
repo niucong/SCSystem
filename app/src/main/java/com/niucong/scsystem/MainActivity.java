07e7c0428e8749366872aa54cc199d1e3325c7e9
牛聪 <niucong@julong.cc>
2017-03-01 16:50:18 +0800

    连接打印机

--- app/src/main/java/com/niucong/scsystem/MainActivity.java
+++ app/src/main/java/com/niucong/scsystem/MainActivity.java
@@ -1,8 +1,17 @@
 package com.niucong.scsystem;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
@@ -11,10 +20,9 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.text.InputType;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -23,19 +31,27 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.gprinter.aidl.GpService;
+import com.gprinter.command.EscCommand;
+import com.gprinter.command.GpCom;
+import com.gprinter.command.GpUtils;
+import com.gprinter.command.LabelCommand;
+import com.gprinter.io.GpDevice;
+import com.gprinter.service.GpPrintService;
 import com.niucong.scsystem.app.App;
 import com.niucong.scsystem.dao.DBUtil;
 import com.niucong.scsystem.dao.DrugInfo;
 import com.niucong.scsystem.dao.SellRecord;
 import com.niucong.scsystem.dao.SellRecordDao;
 import com.niucong.scsystem.dao.StoreList;
+import com.niucong.scsystem.printer.PrinterConnectDialog;
 import com.niucong.scsystem.util.FileUtil;
 import com.niucong.scsystem.view.DividerItemDecoration;
 import com.niucong.scsystem.view.NiftyDialogBuilder;
@@ -47,6 +63,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Vector;
+
+import static com.gprinter.service.GpPrintService.CONNECT_STATUS;
+import static com.niucong.scsystem.printer.ListViewAdapter.DEBUG_TAG;
 
 public class MainActivity extends BasicActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -100,6 +120,8 @@ public class MainActivity extends BasicActivity
         findViewById(R.id.main_btn).setOnClickListener(this);
         mRecyclerView.requestFocus();
 
+        connection();
+        registerReceiver(mBroadcastReceiver, new IntentFilter(GpCom.ACTION_DEVICE_REAL_STATUS));
     }
 
     private void setPayType() {
@@ -433,6 +455,16 @@ public class MainActivity extends BasicActivity
             startActivity(new Intent(this, StatisticsActivity.class));
         } else if (id == R.id.nav_destory) {// 报损处理(自用、损坏或过期、退还商家)
             startActivity(new Intent(this, DestoryActivity.class));
+        } else if (id == R.id.nav_camera) {// 连接打印机
+            if (mGpService == null) {
+                Toast.makeText(this, "Print Service is not start, please check it", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.d(DEBUG_TAG, "openPortConfigurationDialog ");
+                Intent intent = new Intent(this, PrinterConnectDialog.class);
+                boolean[] state = getConnectState();
+                intent.putExtra(CONNECT_STATUS, state);
+                this.startActivity(intent);
+            }
         } else if (id == R.id.nav_camera) {// 设置摄像头
             settingDialog(0);
         } else if (id == R.id.nav_data) {// 导入/导出数据
@@ -557,5 +589,223 @@ public class MainActivity extends BasicActivity
         submitDia.withMessage(null).withDuration(400);
         submitDia.isCancelable(false);
         submitDia.show();
+    }
+
+    private int mPrinterIndex = 0;
+    private GpService mGpService;
+    private PrinterServiceConnection conn = null;
+
+    private static final int MAIN_QUERY_PRINTER_STATUS = 0xfe;
+    private static final int REQUEST_PRINT_LABEL = 0xfd;
+    private static final int REQUEST_PRINT_RECEIPT = 0xfc;
+
+    class PrinterServiceConnection implements ServiceConnection {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+            Log.i(DEBUG_TAG, "onServiceDisconnected() called");
+            mGpService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mGpService = GpService.Stub.asInterface(service);
+        }
+    }
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            // GpCom.ACTION_DEVICE_REAL_STATUS 为广播的IntentFilter
+            if (action.equals(GpCom.ACTION_DEVICE_REAL_STATUS)) {
+
+                // 业务逻辑的请求码，对应哪里查询做什么操作
+                int requestCode = intent.getIntExtra(GpCom.EXTRA_PRINTER_REQUEST_CODE, -1);
+                // 判断请求码，是则进行业务操作
+                if (requestCode == MAIN_QUERY_PRINTER_STATUS) {
+
+                    int status = intent.getIntExtra(GpCom.EXTRA_PRINTER_REAL_STATUS, 16);
+                    String str;
+                    if (status == GpCom.STATE_NO_ERR) {
+                        str = "打印机正常";
+                    } else {
+                        str = "打印机 ";
+                        if ((byte) (status & GpCom.STATE_OFFLINE) > 0) {
+                            str += "脱机";
+                        }
+                        if ((byte) (status & GpCom.STATE_PAPER_ERR) > 0) {
+                            str += "缺纸";
+                        }
+                        if ((byte) (status & GpCom.STATE_COVER_OPEN) > 0) {
+                            str += "打印机开盖";
+                        }
+                        if ((byte) (status & GpCom.STATE_ERR_OCCURS) > 0) {
+                            str += "打印机出错";
+                        }
+                        if ((byte) (status & GpCom.STATE_TIMES_OUT) > 0) {
+                            str += "查询超时";
+                        }
+                    }
+
+                    Toast.makeText(getApplicationContext(), "打印机：" + mPrinterIndex + " 状态：" + str, Toast.LENGTH_SHORT)
+                            .show();
+                } else if (requestCode == REQUEST_PRINT_LABEL) {
+                    int status = intent.getIntExtra(GpCom.EXTRA_PRINTER_REAL_STATUS, 16);
+                    if (status == GpCom.STATE_NO_ERR) {
+                        sendLabel();
+                    } else {
+                        Toast.makeText(MainActivity.this, "query printer status error", Toast.LENGTH_SHORT).show();
+                    }
+                } else if (requestCode == REQUEST_PRINT_RECEIPT) {
+                    int status = intent.getIntExtra(GpCom.EXTRA_PRINTER_REAL_STATUS, 16);
+                    if (status == GpCom.STATE_NO_ERR) {
+                        sendReceipt();
+                    } else {
+                        Toast.makeText(MainActivity.this, "query printer status error", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    };
+
+    void sendReceipt() {
+        EscCommand esc = new EscCommand();
+        esc.addInitializePrinter();
+        esc.addPrintAndFeedLines((byte) 3);
+        esc.addSelectJustification(EscCommand.JUSTIFICATION.CENTER);// 设置打印居中
+        esc.addSelectPrintModes(EscCommand.FONT.FONTA, EscCommand.ENABLE.OFF, EscCommand.ENABLE.ON, EscCommand.ENABLE.ON, EscCommand.ENABLE.OFF);// 设置为倍高倍宽
+        esc.addText("Sample\n"); // 打印文字
+        esc.addPrintAndLineFeed();
+
+		/* 打印文字 */
+        esc.addSelectPrintModes(EscCommand.FONT.FONTA, EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF);// 取消倍高倍宽
+        esc.addSelectJustification(EscCommand.JUSTIFICATION.LEFT);// 设置打印左对齐
+        esc.addText("Print text\n"); // 打印文字
+        esc.addText("Welcome to use SMARNET printer!\n"); // 打印文字
+
+		/* 打印繁体中文 需要打印机支持繁体字库 */
+        String message = "佳博智匯票據打印機\n";
+        // esc.addText(message,"BIG5");
+        esc.addText(message, "GB2312");
+        esc.addPrintAndLineFeed();
+
+		/* 绝对位置 具体详细信息请查看GP58编程手册 */
+        esc.addText("智汇");
+        esc.addSetHorAndVerMotionUnits((byte) 7, (byte) 0);
+        esc.addSetAbsolutePrintPosition((short) 6);
+        esc.addText("网络");
+        esc.addSetAbsolutePrintPosition((short) 10);
+        esc.addText("设备");
+        esc.addPrintAndLineFeed();
+
+		/* 打印图片 */
+        // esc.addText("Print bitmap!\n"); // 打印文字
+        // Bitmap b = BitmapFactory.decodeResource(getResources(),
+        // R.drawable.gprinter);
+        // esc.addRastBitImage(b, b.getWidth(), 0); // 打印图片
+
+		/* 打印一维条码 */
+        esc.addText("Print code128\n"); // 打印文字
+        esc.addSelectPrintingPositionForHRICharacters(EscCommand.HRI_POSITION.BELOW);//
+        // 设置条码可识别字符位置在条码下方
+        esc.addSetBarcodeHeight((byte) 60); // 设置条码高度为60点
+        esc.addSetBarcodeWidth((byte) 1); // 设置条码单元宽度为1
+        esc.addCODE128(esc.genCodeB("SMARNET")); // 打印Code128码
+        esc.addPrintAndLineFeed();
+
+		/*
+         * QRCode命令打印 此命令只在支持QRCode命令打印的机型才能使用。 在不支持二维码指令打印的机型上，则需要发送二维条码图片
+		 */
+        esc.addText("Print QRcode\n"); // 打印文字
+        esc.addSelectErrorCorrectionLevelForQRCode((byte) 0x31); // 设置纠错等级
+        esc.addSelectSizeOfModuleForQRCode((byte) 3);// 设置qrcode模块大小
+        esc.addStoreQRCodeData("www.smarnet.cc");// 设置qrcode内容
+        esc.addPrintQRCode();// 打印QRCode
+        esc.addPrintAndLineFeed();
+
+		/* 打印文字 */
+        esc.addSelectJustification(EscCommand.JUSTIFICATION.CENTER);// 设置打印左对齐
+        esc.addText("Completed!\r\n"); // 打印结束
+        esc.addGeneratePlus(LabelCommand.FOOT.F5, (byte) 255, (byte) 255);
+        // esc.addGeneratePluseAtRealtime(LabelCommand.FOOT.F2, (byte) 8);
+
+        esc.addPrintAndFeedLines((byte) 8);
+
+        Vector<Byte> datas = esc.getCommand(); // 发送数据
+        byte[] bytes = GpUtils.ByteTo_byte(datas);
+        String sss = Base64.encodeToString(bytes, Base64.DEFAULT);
+        int rs;
+        try {
+            rs = mGpService.sendEscCommand(mPrinterIndex, sss);
+            GpCom.ERROR_CODE r = GpCom.ERROR_CODE.values()[rs];
+            if (r != GpCom.ERROR_CODE.SUCCESS) {
+                Toast.makeText(getApplicationContext(), GpCom.getErrorText(r), Toast.LENGTH_SHORT).show();
+            }
+        } catch (RemoteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    void sendLabel() {
+        LabelCommand tsc = new LabelCommand();
+        tsc.addSize(60, 60); // 设置标签尺寸，按照实际尺寸设置
+        tsc.addGap(0); // 设置标签间隙，按照实际尺寸设置，如果为无间隙纸则设置为0
+        tsc.addDirection(LabelCommand.DIRECTION.BACKWARD, LabelCommand.MIRROR.NORMAL);// 设置打印方向
+        tsc.addReference(0, 0);// 设置原点坐标
+        tsc.addTear(EscCommand.ENABLE.ON); // 撕纸模式开启
+        tsc.addCls();// 清除打印缓冲区
+        // 绘制简体中文
+        tsc.addText(20, 20, LabelCommand.FONTTYPE.SIMPLIFIED_CHINESE, LabelCommand.ROTATION.ROTATION_0, LabelCommand.FONTMUL.MUL_1, LabelCommand.FONTMUL.MUL_1,
+                "Welcome to use SMARNET printer!");
+        // 绘制图片
+        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.gprinter);
+        tsc.addBitmap(20, 50, LabelCommand.BITMAP_MODE.OVERWRITE, b.getWidth(), b);
+
+        tsc.addQRCode(250, 80, LabelCommand.EEC.LEVEL_L, 5, LabelCommand.ROTATION.ROTATION_0, " www.smarnet.cc");
+        // 绘制一维条码
+        tsc.add1DBarcode(20, 250, LabelCommand.BARCODETYPE.CODE128, 100, LabelCommand.READABEL.EANBEL, LabelCommand.ROTATION.ROTATION_0, "SMARNET");
+        tsc.addPrint(1, 1); // 打印标签
+        tsc.addSound(2, 100); // 打印标签后 蜂鸣器响
+        tsc.addCashdrwer(LabelCommand.FOOT.F5, 255, 255);
+        Vector<Byte> datas = tsc.getCommand(); // 发送数据
+        byte[] bytes = GpUtils.ByteTo_byte(datas);
+        String str = Base64.encodeToString(bytes, Base64.DEFAULT);
+        int rel;
+        try {
+            rel = mGpService.sendLabelCommand(mPrinterIndex, str);
+            GpCom.ERROR_CODE r = GpCom.ERROR_CODE.values()[rel];
+            if (r != GpCom.ERROR_CODE.SUCCESS) {
+                Toast.makeText(getApplicationContext(), GpCom.getErrorText(r), Toast.LENGTH_SHORT).show();
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void connection() {
+        conn = new PrinterServiceConnection();
+        Log.i(DEBUG_TAG, "connection");
+        Intent intent = new Intent(this, GpPrintService.class);
+        bindService(intent, conn, Context.BIND_AUTO_CREATE); // bindService
+    }
+
+    public boolean[] getConnectState() {
+        boolean[] state = new boolean[GpPrintService.MAX_PRINTER_CNT];
+        for (int i = 0; i < GpPrintService.MAX_PRINTER_CNT; i++) {
+            state[i] = false;
+        }
+        for (int i = 0; i < GpPrintService.MAX_PRINTER_CNT; i++) {
+            try {
+                if (mGpService.getPrinterConnectStatus(i) == GpDevice.STATE_CONNECTED) {
+                    state[i] = true;
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+        return state;
     }
 }
